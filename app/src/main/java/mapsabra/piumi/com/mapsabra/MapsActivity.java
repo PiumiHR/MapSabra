9decef101f290c55379aa47e9ed09faeaaaa86ae
PiumiHR <rathnayakepiumi19@gmail.com>
2016-06-24 16:01:57 +0530

    new..search

--- app/src/main/java/mapsabra/piumi/com/mapsabra/MapsActivity.java
+++ app/src/main/java/mapsabra/piumi/com/mapsabra/MapsActivity.java
@@ -2,6 +2,8 @@ package mapsabra.piumi.com.mapsabra;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +29,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mapFragment.getMapAsync(this);
         }
 
+    //searching part
+    public void onSearch(View view){
+
+       EditText location_tf = (EditText) findViewById(R.id.etAddress);
+        String location = location_tf.getText().toString();
+
+    }
+
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
@@ -41,6 +51,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // Add a marker in sabaragamuwa university and move the camera
             LatLng sabra = new LatLng(6.714682, 80.789314);
             LatLng sabraAdministrativeArea = new LatLng(6.714028, 80.788647);
+            LatLng sabraGround = new LatLng(6.710839, 80.789654);
+            LatLng swimmingPool = new LatLng(6.714533, 80.786837);// should implement
+            LatLng hunduwaPlayGround = new LatLng(6.715319, 80.787561);// should implement
+            LatLng tennisCourt = new LatLng(6.714563, 80.787256);// should implement
+            LatLng panibaratha = new LatLng(6.713432, 80.788209);// should implement
+            LatLng quatus = new LatLng(6.712787, 80.788772);// should implement
+            LatLng quatus2 = new LatLng(6.712406, 80.789130);// should implement
             mMap.addMarker(new MarkerOptions()
                     .position(sabra)
                     .title("Marker in sabragamuwa university"));
@@ -48,6 +65,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .position(sabraAdministrativeArea)
                     .title("Administrative area")
                     .snippet("Administrative area")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_plusone_small_off_client)));
+
+            //swimming pool
+            mMap.addMarker(new MarkerOptions()
+                    .position(swimmingPool)
+                    .title("Swimming Pool")
+                    .snippet("Swimming Pool")
+                    );
+
+            //hunduwa playground. Have to change name
+            mMap.addMarker(new MarkerOptions()
+                            .position(hunduwaPlayGround)
+                            .title("Hunduwa")
+                            .snippet("Hunduwa")
+            );
+
+            //tennis court
+            mMap.addMarker(new MarkerOptions()
+                            .position(tennisCourt)
+                            .title("Tennis Court")
+                            .snippet("Tennis Court")
+            );
+
+            //panibharatha.Have to change name
+            mMap.addMarker(new MarkerOptions()
+                            .position(panibaratha)
+                            .title("Panibharatha")
+                            .snippet("Panibharatha")
+            );
+
+            //quatus 1 chnge the name
+            mMap.addMarker(new MarkerOptions()
+                            .position(quatus)
+                            .title("quatus")
+                            .snippet("quatus")
+            );
+
+            //quatus 2 chnge the name
+            mMap.addMarker(new MarkerOptions()
+                            .position(quatus2)
+                            .title("quatus2")
+                            .snippet("quatus2")
+            );
+
+//.icon(BitmapDescriptorFactory.fromResource(R.drawable.pool))
+            mMap.addMarker(new MarkerOptions()
+                    .position(sabraGround)
+                    .title("University Ground")
+                    .snippet("University Ground")
                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_plusone_small_off_client)));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sabra, 15));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
